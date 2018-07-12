99bd7803f4e2cd02dfe308ebe068cea28cad93ab
csergey8 <c.sergey@protonmail.com>
2018-07-12 15:28:50 +0300

    added movieList component

---
+++ src/modules/movie-browser/movie-list/movie-list.component.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import MovieCard from '../movie-card/movie-card.component';
+import Loader from '../../common/loader.component';
+
+const styles = {
+  movieColumn: {
+    margingBottom: 20
+  }
+}
+
+const MovieList = ({movies, isLoading}) => {
+  const movieColumn = movies ? movies.map(movie => (
+    <Col style={styles.movieColumn} key={movie.id} xs={12} sm={4} md={3} lg={3}>
+      <MovieCard movie={movie} />
+    </Col>
+  )) : null;
+  return (
+    <Row>
+      {movieColumn}
+      <Loader />
+    </Row>
+  )
+}
+
+
+export default MovieList;
