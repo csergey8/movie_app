0baaa409a51a020a63f894cd8d212a6dc0d786fc
csergey8 <c.sergey@protonmail.com>
2018-07-12 12:52:11 +0300

    git add search reducers

--- src/modules/movie-browser/movie-browser.reducers.js
+++ src/modules/movie-browser/movie-browser.reducers.js
@@ -1,12 +1,47 @@
 import { combineReducers } from 'redux';
-import { createReducer } from '../common/redux.helpers';
-
+import { createReducer, createAsyncReducer } from '../common/redux.helpers';
+import { keys as movieActionKeys } from './movie-browser.actions';
 const movieModalReducer = createReducer({ isOpen: false }, {
 
 });
 
+
+const movieSuccessReducer = (state, action) => {
+  const existingMovies = state.response ? state.response.results : [];
+
+  return {
+    ...state,
+    isLoading: false,
+    response: {
+      ...action.response,
+      results: [
+        ...existingMovies,
+        ...action.response.results
+      ]
+    }
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
 const movieBrowserReducer = combineReducers({
-  movieModal: movieModalReducer
+  movieModal: movieModalReducer,
+  topMovies: createAsyncReducer(movieActionKeys.GET_TOP_MOVIES, {
+    [`${movieActionKeys.GET_TOP_MOVIES}_SUCCESS`]: movieSuccessReducer 
+  }),
+  movieSearch: createAsyncReducer(movieActionKeys.SEARCH_MOVIES, {
+    [`${movieActionKeys.SEARCH_MOVIES}_SUCCESS`]: movieSuccessReducer
+  }),
+  movieDetails: createAsyncReducer(movieActionKeys.GET_MOVIE_DETAILS)
 })
 
 export default movieBrowserReducer;
